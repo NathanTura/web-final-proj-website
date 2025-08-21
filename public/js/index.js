0cc4a37fb36b7961607e6dcfa73805738119836a
nathantura <you@example.com>
2025-08-21 23:01:30 +0300

    update on index

---
+++ public/js/index.js
@@ -0,0 +1,35 @@
+
+document.addEventListener('DOMContentLoaded' , ()=>{
+    console.log("working")
+});
+
+
+window.addEventListener("resize", () => { 
+      const m_list = document.getElementById('menulists')
+     if (window.innerWidth > 769) {
+        m_list.style.margin = 'auto'
+        m_list.style.display = 'flex'     
+      
+    }
+    else{
+         m_list.style.display = 'none'
+             isshowing = false
+    }
+})
+
+
+var isshowing = false
+function showmenu(){
+    const m_list = document.getElementById('menulists')
+    if (!isshowing){
+        m_list.style.margin = '20px'
+        m_list.style.marginTop = '10px'
+        m_list.style.display = 'flex'
+        isshowing = !isshowing
+    }
+    else{
+   
+        m_list.style.display = 'none'
+        isshowing = !isshowing
+    }
+}
