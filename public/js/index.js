00d77909a8238c5bbc64ee18cbbd18a14467f9da
nathantura <you@example.com>
2025-08-21 23:16:27 +0300

    update index

--- public/js/index.js
+++ public/js/index.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded' , ()=>{
 window.addEventListener("resize", () => { 
       const m_list = document.getElementById('menulists')
      if (window.innerWidth > 769) {
-        m_list.style.margin = 'auto'
+     
         m_list.style.display = 'flex'     
       
     }
